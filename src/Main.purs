0bf4258952fa5966760758ecc8a873010401eb92
arsdragonfly <arsdragonfly@gmail.com>
2019-12-25 21:50:07 -0500

    blyat

--- src/Main.purs
+++ src/Main.purs
@@ -2,10 +2,15 @@ module Main where
 
 import Effect (Effect)
 import Effect.Console (log)
-import Prelude (Unit, bind, ($), (<>), show)
+import Prelude (Unit, bind, ($), (<>), show, discard, void)
 import Purescreeps.Spawn (createCreepsForAllColonies)
+import Data.Traversable (traverse)
+import Screeps.Creep (say)
+import Screeps.Game (creeps)
 
 main :: Effect Unit
 main = do
   results <- createCreepsForAllColonies
   log $ "Start: " <> show results
+  map <- creeps
+  void $ traverse (\c -> say c "blyat!") map
